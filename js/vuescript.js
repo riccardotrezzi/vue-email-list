0981b351111a2f6789e609e7699325f2e7bf935c
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-09-30 17:25:40 +0200

    esercizio base fatto

--- js/vuescript.js
+++ js/vuescript.js
@@ -1,9 +1,25 @@
 const { createApp } = Vue
 
-  createApp({
+  const app = createApp({
     data() {
       return {
-        message: 'Vue Email List'
+        emails: []
+      }; 
+    },
+    created(){
+
+      for(let i=0; i < 10; i++){
+        axios
+        .get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+
+            const email = res.data.response;
+            console.log('Email ' + i + ':', email);
+
+            
+            this.emails.push(email);
+        });
       }
+      
     }
   }).mount('#app');
